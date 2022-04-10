81e23c782d6551c1cf37dc33b49bb0add4925f9e
Kofu145g <kofu145g@gmail.com>
2022-04-10 16:15:56 -0700

    ECS template
    Basic entity and component

---
+++ Swordfish/ECS/Entity.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Swordfish.ECS
+{
+    public class Entity
+    {
+        // use arrays for cache efficiency
+        private readonly ConcurrentDictionary<Type, IComponent> components;
+        public readonly Guid id;
+        public Entity(Guid id)
+        {
+            this.id = id;
+            this.components = new ConcurrentDictionary<Type, IComponent>();
+        }
+        // class, specifies t has to be a class
+        public T GetComponent<T>() where T : class, IComponent
+        {
+            return components.ContainsKey(typeof(T)) ? components[typeof(T)] as T : null;
+        }
+
+        public bool HasComponent<T>() where T : IComponent
+        {
+            return components.ContainsKey(typeof(T));
+        }
+
+        public void AddComponent<T>(T component) where T : IComponent
+        {
+            components[typeof(T)] = component;
+        }
+
+    }
+}
---
+++ Swordfish/ECS/IComponent.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Swordfish.ECS
+{
+    public interface IComponent
+    {
+        public void OnLoad();
+
+        public void Update();
+
+    }
+}

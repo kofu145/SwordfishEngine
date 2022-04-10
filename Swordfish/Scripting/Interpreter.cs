f780d9bc12f931156634f02b43f30764898f1eab
dnfd <dynamicfire069@gmail.com>
2022-04-10 15:56:50 -0700

    Set up folder for scripting

---
+++ Swordfish/Scripting/Interpreter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using IronPython.Runtime;
+namespace Swordfish.Scripting
+{
+    internal class Interpreter
+    {
+
+        
+    }
+}

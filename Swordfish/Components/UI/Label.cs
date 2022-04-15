a47aa7e2e120e41116ad51b67bdffd156f7abbd3
Kofu145g <kofu145g@gmail.com>
2022-04-14 18:12:42 -0700

    Label and UI > components move
    Moved the UI directory under components, and added a label class

---
+++ Swordfish/Components/UI/Label.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Swordfish.ECS;
+
+namespace Swordfish.Components
+{
+    public class Label : IComponent
+    {
+        private TextStyle _style;
+        private string _text;
+        public Label(string text)
+        {
+            _text = text;
+        }
+        public Label(string text, TextStyle style)
+        {
+            _text = text;
+            _style = style;
+        }
+
+        public void OnLoad()
+        {
+        }
+        public void Update()
+        {
+        }
+    }
+}

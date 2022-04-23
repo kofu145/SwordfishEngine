74e61167eea61c3afb09e819f0fa15e5d1970f86
dnfd <dynamicfire069@gmail.com>
2022-04-22 23:58:52 -0700

    add openal32.dll

---
+++ Swordfish/py/testScript.py
@@ -0,0 +1,13 @@
+﻿component = System.getComponent("Label", Entity0)
+transform = System.getComponent("Transform", Entity0)
+if component.Color.X > 1:
+	component.Color = System.newVec3(0, component.Color.Y + 0.005,component.Color.Z + 0.002)
+else: 
+	if component.Color.Y > 1:
+		component.Color = System.newVec3(component.Color.X + 0.001, 0,component.Color.Z + 0.002)
+	else: 
+		if component.Color.Z > 1:
+			component.Color = System.newVec3(component.Color.X + 0.001, component.Color.Y + 0.005, 0)
+
+transform.Rotation = System.newVec3(0, 0, transform.Rotation.Z + 0.1)
+component.Text = "727"
